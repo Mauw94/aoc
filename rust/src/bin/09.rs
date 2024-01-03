ceddf07d98c49a4fb0ee99e2b02d27608e58db20
Maurits <mauritsseelen@hotmail.com>
2024-01-03 11:40:27 +0100

    day9

--- rust/src/bin/09.rs
+++ rust/src/bin/09.rs
@@ -2,13 +2,16 @@ advent_of_code::solution!(9);
 
 pub fn part_one(input: &str) -> Option<i32> {
     let lists = parse_lists(input);
-    let histories = calculate_histories(lists);
+    let histories = calculate_histories(lists, true);
 
     Some(histories.iter().sum())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    let lists = parse_lists(input);
+    let histories = calculate_histories(lists, false);
+
+    Some(histories.iter().sum())
 }
 
 fn parse_lists(input: &str) -> Vec<Vec<i32>> {
@@ -22,16 +25,36 @@ fn parse_lists(input: &str) -> Vec<Vec<i32>> {
     n
 }
 
-fn calculate_histories(lists: Vec<Vec<i32>>) -> Vec<i32> {
+fn calculate_histories(lists: Vec<Vec<i32>>, part1: bool) -> Vec<i32> {
     let mut histories: Vec<i32> = Vec::new();
     for list in lists {
+        let history: Vec<i32>;
         let mut previous: Vec<i32> = Vec::new();
-        previous.push(list.last().unwrap().clone());
-        let history: Vec<i32> = subtract_until_zero(list, previous);
-        histories.push(history.iter().sum::<i32>());
+        let extrapolated: i32;
+
+        if part1 {
+            previous.push(list.last().unwrap().clone());
+            history = subtract_until_zero(list, previous);
+            histories.push(history.iter().sum::<i32>());
+        } else {
+            previous.push(list.first().unwrap().clone());
+            history = subtract_until_zero_reversed(list, previous);
+            extrapolated = calc_extrapolated_value(&history);
+            histories.push(extrapolated);
+        }
     }
 
     histories
+}
+
+fn calc_extrapolated_value(history: &Vec<i32>) -> i32 {
+    let mut previous: i32 = 0;
+
+    for (i, _) in history.iter().enumerate().rev() {
+        previous = history[i] - previous;
+    }
+
+    previous
 }
 
 fn subtract_until_zero(list: Vec<i32>, mut previous: Vec<i32>) -> Vec<i32> {
@@ -54,6 +77,26 @@ fn subtract_until_zero(list: Vec<i32>, mut previous: Vec<i32>) -> Vec<i32> {
     return subtract_until_zero(converted, previous);
 }
 
+fn subtract_until_zero_reversed(list: Vec<i32>, mut previous: Vec<i32>) -> Vec<i32> {
+    let mut converted: Vec<i32> = Vec::new();
+
+    for (i, _) in list.iter().enumerate().rev() {
+        if i > 0 {
+            let x = list[i] - list[i - 1];
+            converted.insert(0, x);
+        }
+    }
+
+    let zero_summed: i32 = converted.iter().sum();
+    if zero_summed == 0 {
+        return previous;
+    }
+
+    previous.push(converted.first().unwrap().clone());
+
+    return subtract_until_zero_reversed(converted, previous);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,6 +110,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
